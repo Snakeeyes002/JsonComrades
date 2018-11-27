af4399be69f8946cdd322539ef60d52ee085540f
Kosogov <i.kosogov@gmail.com>
2018-11-27 12:07:29 +0200

    Create class Post

--- Forms/FormComments.cs
+++ Forms/FormComments.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,11 @@ namespace JsonComrades.Forms
         public FormComments()
         {
             InitializeComponent();
+        }
+
+        private void FormComments_Load(object sender, EventArgs e)
+        {
+        
         }
     }
 }
